790e55cd059ce556c754606613a99104a0e12854
YJLkey <yesyjl@163.com>
2019-12-13 09:21:06 +0800

    注释更正

--- day_10/tolset/harib07a/keyboard.c
+++ day_10/tolset/harib07a/keyboard.c
@@ -6,7 +6,7 @@ struct FIFO8 keyfifo;
 void inthandler21(int *esp)
 {
 	unsigned char data;
-	io_out8(PIC0_OCW2, 0x61);	/* IRQ-01受付完了をPICに通知 */
+	io_out8(PIC0_OCW2, 0x61);	/* 通知PIC IRQ-01的受理已经完成 */
 	data = io_in8(PORT_KEYDAT);
 	fifo8_put(&keyfifo, data);
 	return;
